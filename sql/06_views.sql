5e179b4039873c4091546c45c470b775efe1af87
karna098 <somasekharsanti@gmail.com>
2026-07-25 23:20:56 +0530

    Create reusable reporting views

---
+++ sql/06_views.sql
@@ -0,0 +1,11 @@
+/*
+ ===============================================================================
+ Project      : FlowStack SaaS Analytics
+ File         : 06_views.sql
+ 
+ Description  :
+ Creates reusable SQL views for reporting,
+ dashboard development, and business analysis.
+ ===============================================================================
+ */
+USE flowstack_analytics;
